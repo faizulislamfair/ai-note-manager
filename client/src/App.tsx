8eb5cfb37352822326de2f42bc16f5f035437896
faizulislamfair <faizulislamfair@gmail.com>
2025-07-19 00:52:08 +0600

    add MUI custom theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,57 +1,61 @@
-import { Box, Typography } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider, Typography } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import NotFound from './pages/NotFound';
 import Dashboard from "./pages/Dashboard";
 import AppLayout from "./components/layout/AppLayout";
+import { theme } from "./ui/theme";
 
 
 function App() {
   return (
-   <Routes>
-    <Route
-    path="/"
-    element={
-     <AppLayout />
-    }
-    >
-      <Route 
-      index
-      element={
-      <Dashboard />
-      }
-      />
-      <Route
-        path="bookmarks"
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route
+        path="/"
         element={
-          <Box>
-          <Typography variant="h1">Bookmarks</Typography>
-        </Box>
+        <AppLayout />
         }
-      />
-      <Route
-        path="archive"
-        element={
-          <Box>
-          <Typography variant="h1">Archive</Typography>
-        </Box>
-        }
-      />
-      <Route
-        path="settings"
-        element={
-          <Box>
-          <Typography variant="h1">Settings</Typography>
-        </Box>
-        }
-      />
-    </Route>
-    <Route
-      path="*"
-      element={
-       <NotFound/>
-      }
-    />
-   </Routes>
+        >
+          <Route 
+          index
+          element={
+          <Dashboard />
+          }
+          />
+          <Route
+            path="bookmarks"
+            element={
+              <Box>
+              <Typography variant="h1">Bookmarks</Typography>
+            </Box>
+            }
+          />
+          <Route
+            path="archive"
+            element={
+              <Box>
+              <Typography variant="h1">Archive</Typography>
+            </Box>
+            }
+          />
+          <Route
+            path="settings"
+            element={
+              <Box>
+              <Typography variant="h1">Settings</Typography>
+            </Box>
+            }
+          />
+        </Route>
+        <Route
+          path="*"
+          element={
+          <NotFound/>
+          }
+        />
+      </Routes>
+   </ThemeProvider>
   )
 }
 
